ce72e22e95102952418690045eada107098889a7
Ahmed Batyrov <ahmed.batyrovv@gmail.com>
2026-06-09 09:36:06 +0300

    updated number

--- src/data/payments.js
+++ src/data/payments.js
@@ -4,9 +4,6 @@ export const terminalNumbers = [
 ]
 
 export const smsNumbers = [
-  { id: 1, number: '+993 65 111 222', label: 'SMS #1', amount: '50' },
-  { id: 2, number: '+993 65 333 444', label: 'SMS #2', amount: '100' },
-  { id: 3, number: '+993 65 555 666', label: 'SMS #3', amount: '150' },
-  { id: 4, number: '+993 65 777 888', label: 'SMS #4', amount: '200' },
-  { id: 5, number: '+993 65 999 000', label: 'SMS #5', amount: '250' },
-]
+  { id: 1, number: '+993 61 772671', label: 'SMS #1', , amount: '0' },
+  { id: 2, number: '+993 64 614588', label: 'SMS #2', amount: '0' },
+  ]
